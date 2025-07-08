808fe7232f584093868323812a89d0b02026577f
liquid <13621351+liqtags@users.noreply.github.com>
2025-07-08 01:50:39 +0100

    add

--- main.ts
+++ main.ts
@@ -6,6 +6,14 @@ export function subtract(a: number, b: number): number {
   return a - b;
 }
 
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function divide(a: number, b: number): number {
+  return a / b;
+}
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
